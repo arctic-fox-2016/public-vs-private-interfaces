d201ad081a711f0dbd8a8f8b9851838ca2c01904
Ahmad Ahmadi & Imam <pair+ahmadahmadi14.imamnugraha23@hacktiv8.com>
2016-09-01 12:52:54 +0700

    ahmadi & imam

--- BankAccount.js
+++ BankAccount.js
@@ -1,21 +1,42 @@
 "use strict"
 
 // release 0
-const ACCT_NUMBER = new ChangeThis()
+//const ACCT_NUMBER = new ChangeThis()
 
 class BankAccount {
-  constructor(customer_name, type, acct_number) {}
+  constructor(customer_name, type, acct_number) {
+    this.customer_name = customer_name;
 
-  to_s() {}
+    this.type = type;
 
-  cover_digits() {}
+    this._acct_number = acct_number;
+  }
+  get accountNumber(){
+    return this._acct_number;
+  }
+  set accountNumber(number){
+    this._acct_number = number;
+  }
+
+  //console.log = acct_number ("hacktivate", "Checking","333-555-888")
+
+
+  to_s(){
+    console.log(this.customer_name, this.type, this._acct_number);
+
+  }
+
+  cover_digits() {
+    let cover = this._acct_number.replace(/(\d{3}).*(\d{3}).*(\d{3})/g,"***-***-$3")
+    console.log(this.customer_name + " : " + this.type + " # "+ cover);
+  }
+
 }
-
-let my_acct = new BankAccount("Name", "Checking", "000-000-000")
+let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
 console.log(my_acct)
-console.log(my_acct.ACCT_NUMBER.get(this))
+console.log(my_acct.accountNumber)
 
-// release 1
+//release 1
 my_acct.to_s() // "Name: Checking# 000-000-000"
 my_acct.cover_digits() // "Name: Checking# ***-***-000"
