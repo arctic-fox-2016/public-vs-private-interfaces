d92cf57aa75d55bfc3789cdb3b400c23399300b7
Dash O'Pepper & Kun Anggiar Lanang &   "name": null <pair+-.kun7.tevin@hacktiv8.com>
2016-09-01 11:37:48 +0700

    kelar juga

--- BankAccount.js
+++ BankAccount.js
@@ -1,21 +1,54 @@
 "use strict"
 
-// release 0
-const ACCT_NUMBER = new ChangeThis()
+// // release 0
+//const ACCT_NUMBER = new ChangeThis()
 
 class BankAccount {
-  constructor(customer_name, type, acct_number) {}
+  constructor(customer_name, type, acct_number) {
+    this.customer_name = customer_name;
+    this.type          = type;
+    this._acct_number   = acct_number;
+  }
 
-  to_s() {}
+  get acctNumber(){
+    return this._acct_number
+  }
 
-  cover_digits() {}
+  set acctNumber(number){
+    this._acct_number = number
+  }
+
+  to_s() {
+    console.log(this.customer_name + ": " + this.type +"# "
+    + this._acct_number)
+  }
+
+  cover_digits() {
+    let hilang = this._acct_number.replace(/(\d{3}).*(\d{3}).*(\d{3})/g, "***-***-$3")
+    console.log(this.customer_name + ": " + this.type + "# " + hilang)
+
+  }
 }
 
-let my_acct = new BankAccount("Name", "Checking", "000-000-000")
+let my_acct = new BankAccount("BLublu", "Checking", "317-820-087")
 
 console.log(my_acct)
-console.log(my_acct.ACCT_NUMBER.get(this))
+console.log(my_acct.acctNumber)
+//console.log(my_acct.ACCT_NUMBER.get(this))
 
-// release 1
+// // release 1
 my_acct.to_s() // "Name: Checking# 000-000-000"
 my_acct.cover_digits() // "Name: Checking# ***-***-000"
+
+// class Person{
+//   constructor(name){
+//     this._name =name;
+//   }
+//   get name(){
+//     return this._name;
+//   }
+//   set name(newName){
+//     this._name = newName;
+//   }
+//   BankAccount
+// }
