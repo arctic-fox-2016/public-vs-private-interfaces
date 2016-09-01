ae750eb93a6b879db36534258341783491246274
Elkana Lumbantobing & Peter Raswono <pair+elkana-ltobing.praswono@hacktiv8.com>
2016-09-01 11:18:31 +0700

    Elkana & Peter

--- BankAccount.js
+++ BankAccount.js
@@ -1,20 +1,32 @@
 "use strict"
 
 // release 0
-const ACCT_NUMBER = new ChangeThis()
+const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
-  constructor(customer_name, type, acct_number) {}
+  constructor(customer_name, type, acct_number) {
+    this._customer_name = customer_name;
+    this._type = type;
+    ACCT_NUMBER.set(this, acct_number);
+  }
 
-  to_s() {}
+  to_s() {
+    console.log(this._customer_name + ": " + this._type + "# " + ACCT_NUMBER.get(this));
+  }
 
-  cover_digits() {}
+  cover_digits() {
+    console.log(this._customer_name + ": " + this._type + "# " + ACCT_NUMBER.get(this).replace(/\d{3}-\d{3}-(\d{3})/g, "***-***-$1"));
+  }
+
+  get acct_number() {
+    return ACCT_NUMBER.get(this);
+  }
 }
 
-let my_acct = new BankAccount("Name", "Checking", "000-000-000")
+let my_acct = new BankAccount("Hacktiv8", "Checking", "333-555-888")
 
-console.log(my_acct)
-console.log(my_acct.ACCT_NUMBER.get(this))
+console.log(my_acct);
+console.log(my_acct.acct_number);
 
 // release 1
 my_acct.to_s() // "Name: Checking# 000-000-000"
