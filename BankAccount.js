47f4ea4d77c3a4a5c2d751683abdbbe0c9d1f888
Ivan Gerard & Ryan Andhaka <pair+ivangerard.ryanandhaka@hacktiv8.com>
2016-09-01 20:33:37 +0700

    Public Private

--- BankAccount.js
+++ BankAccount.js
@@ -1,21 +1,66 @@
 "use strict"
 
 // release 0
-const ACCT_NUMBER = new ChangeThis()
+
 
 class BankAccount {
-  constructor(customer_name, type, acct_number) {}
+  constructor(customer_name, type, acct_number) {
+    this._customer_name = customer_name
+    this._type = type
+    this._acct_number = acct_number
+  }
 
-  to_s() {}
+  to_s() {
+    return "Hacktivate :" +this._type+"#"+this._acct_number
+  }
 
-  cover_digits() {}
+  get customer_name(){
+    return this._customer_name
+  }
+
+  set customer_name(customer){
+    return this._customer_name = customer
+  }
+
+  get type(){
+    return this._type
+  }
+  set type(type){
+    return this._type = type
+  }
+
+  set acct_number(acct){
+    return this._acct_number = acct
+  }
+  get acct_number(){
+    return this._acct_number
+  }
+
+  cover_digits() {
+    var tmp = this._acct_number.match(/\d\d\d[-]\d\d\d/g);
+    var toString = tmp.toString()
+    toString = toString.replace(toString,"XXX-XXX") + '-' + this._acct_number.substring(8,11)
+    return toString;
+  }
 }
 
 let my_acct = new BankAccount("Name", "Checking", "000-000-000")
 
-console.log(my_acct)
-console.log(my_acct.ACCT_NUMBER.get(this))
+//
+// console.log(my_acct)
+// console.log(my_acct.to_s());
+// console.log(my_acct.account);
+my_acct.account = "ivangerards"
+console.log(my_acct.account);
+// console.log(my_acct.getAccount);
+// console.log(my_acct.ACCT_NUMBER.get(this))
+//
+// // release 1
 
-// release 1
-my_acct.to_s() // "Name: Checking# 000-000-000"
-my_acct.cover_digits() // "Name: Checking# ***-***-000"
+console.log(my_acct.to_s());
+
+console.log(my_acct.cover_digits());
+// var card = ["000-000-000"]
+// card.split(card)
+//
+// console.log(card);
